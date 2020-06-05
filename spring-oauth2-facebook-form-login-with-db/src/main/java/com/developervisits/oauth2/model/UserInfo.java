db6bbab89129c64303d10dbc98fb90d988f9aa74
rajeevshukla <mail2rajeevshukla@gmail.com>
2020-06-04 23:29:14 -0500

    Adding user info

---
+++ spring-oauth2-facebook-form-login-with-db/src/main/java/com/developervisits/oauth2/model/UserInfo.java
@@ -0,0 +1,12 @@
+package com.developervisits.oauth2.model;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+   
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String id;
+}
